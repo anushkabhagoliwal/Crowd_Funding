f9b2b1749448c07406c1d625ca189fe2fcf581f2
Anushka Bhagoliwal <143029316+anushkabhagoliwal@users.noreply.github.com>
2024-07-01 21:38:05 +0530

    Update Frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+import { DisplayCampaigns } from '../components'; 
+import { useStateContext } from '../context'
+
+const Home = () => {
+  const [isaLoading, setfirst] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns }= 
+  useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+     if(contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns 
+     title="All Campaigns"
+     isaLoading= {isaLoading}
+     campaigns={campaigns}
+    />
+  )
+}
+
+export default Home
